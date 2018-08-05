1e4efd2246491801069ba31c21ccb53715832cd2
bake <bake@192k.pw>
2018-08-05 11:41:23 +0200

    renamed variables

--- gotumblr.go
+++ gotumblr.go
@@ -4,9 +4,7 @@ package gotumblr
 
 import (
 	"encoding/json"
-	"errors"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"net/url"
 
@@ -89,7 +87,7 @@ func New(consumerKey, consumerSecret, oauthToken, oauthSecret string, setters ..
 
 // Info gets the user information.
 func (c *Client) Info() (UserInfoResponse, error) {
-	data, err := c.Get("/v2/user/info", url.Values{})
+	data, err := c.Get("/v2/user/info", nil)
 	if err != nil {
 		return UserInfoResponse{}, err
 	}
@@ -100,27 +98,12 @@ func (c *Client) Info() (UserInfoResponse, error) {
 // Avatar retrieves the url of the blog's avatar.
 // size can be: 16, 24, 30, 40, 48, 64, 96, 128 or 512.
 func (c *Client) Avatar(blogname string, size int) (AvatarResponse, error) {
-	requestURL := c.host + fmt.Sprintf("/v2/blog/%s/avatar/%d", blogname, size)
-	httpRequest, err := http.NewRequest("GET", requestURL, nil)
+	data, err := c.Get(fmt.Sprintf("/v2/blog/%s/avatar/%d", blogname, size), nil)
 	if err != nil {
 		return AvatarResponse{}, err
 	}
-	transport := &http.Transport{}
-	httpResponse, err := transport.RoundTrip(httpRequest)
-	if err != nil {
-		return AvatarResponse{}, err
-	}
-	defer httpResponse.Body.Close()
-	body, err := ioutil.ReadAll(httpResponse.Body)
-	if err != nil {
-		return AvatarResponse{}, err
-	}
-	var data CompleteResponse
-	if err := json.Unmarshal(body, &data); err != nil {
-		return AvatarResponse{}, err
-	}
-	var result AvatarResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res AvatarResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Likes gets the likes of the given user.
@@ -132,8 +115,8 @@ func (c *Client) Likes(options url.Values) (LikesResponse, error) {
 	if err != nil {
 		return LikesResponse{}, err
 	}
-	var result LikesResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res LikesResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Following gets the blogs that the user is following.
@@ -145,8 +128,8 @@ func (c *Client) Following(options url.Values) (FollowingResponse, error) {
 	if err != nil {
 		return FollowingResponse{}, err
 	}
-	var result FollowingResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res FollowingResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Dashboard gets the dashboard of the user.
@@ -162,8 +145,8 @@ func (c *Client) Dashboard(options url.Values) (DraftsResponse, error) {
 	if err != nil {
 		return DraftsResponse{}, err
 	}
-	var result DraftsResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res DraftsResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Tagged gets a list of posts with the given tag.
@@ -179,8 +162,8 @@ func (c *Client) Tagged(tag string, options url.Values) ([]json.RawMessage, erro
 	if err != nil {
 		return []json.RawMessage{}, err
 	}
-	result := []json.RawMessage{}
-	return result, json.Unmarshal(data.Response, &result)
+	res := []json.RawMessage{}
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Posts gets a list of posts from a blog.
@@ -194,17 +177,17 @@ func (c *Client) Tagged(tag string, options url.Values) ([]json.RawMessage, erro
 // offset: the number of the post you want to start from;
 // filter: return only posts with a specific format(e.g. html, text, raw).
 func (c *Client) Posts(blogname, postsType string, options url.Values) (PostsResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/posts", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/posts", blogname)
 	if postsType != "" {
-		requestURL += "/" + postsType
+		reqURL += "/" + postsType
 	}
 	options.Set("api_key", c.apiKey)
-	data, err := c.Get(requestURL, options)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return PostsResponse{}, err
 	}
-	var result PostsResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res PostsResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Notes gets a list of notes on a post.
@@ -212,28 +195,28 @@ func (c *Client) Posts(blogname, postsType string, options url.Values) (PostsRes
 // mode: likes, reblogs_with_tags or rollup
 // before_timestamp: return only notes created before the given timestamp
 func (c *Client) Notes(blogname, postID string, options url.Values) (NotesResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/notes", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/notes", blogname)
 	options.Set("api_key", c.apiKey)
 	options.Set("id", postID)
-	data, err := c.Get(requestURL, options)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return NotesResponse{}, err
 	}
-	var result NotesResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res NotesResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // BlogInfo gets general information about the blog.
 // blogname: name of the blog you want to get information about(e.g. mgterzieva.tumblr.com).
 func (c *Client) BlogInfo(blogname string) (BlogInfoResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/info", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/info", blogname)
 	options := url.Values{"api_key": []string{c.apiKey}}
-	data, err := c.Get(requestURL, options)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return BlogInfoResponse{}, err
 	}
-	var result BlogInfoResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res BlogInfoResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Followers gets the followers of the blog given.
@@ -242,13 +225,13 @@ func (c *Client) BlogInfo(blogname string) (BlogInfoResponse, error) {
 // limit: the number of results to return, inclusive;
 // offset: result to start at.
 func (c *Client) Followers(blogname string, options url.Values) (FollowersResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/followers", blogname)
-	data, err := c.Get(requestURL, options)
+	reqURL := fmt.Sprintf("/v2/blog/%s/followers", blogname)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return FollowersResponse{}, err
 	}
-	var result FollowersResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res FollowersResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // BlogLikes gets the likes of blog given.
@@ -257,14 +240,14 @@ func (c *Client) Followers(blogname string, options url.Values) (FollowersRespon
 // limit: how many likes do you want to get;
 // offset: the number of the like you want to start from.
 func (c *Client) BlogLikes(blogname string, options url.Values) (LikesResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/likes", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/likes", blogname)
 	options.Set("api_key", c.apiKey)
-	data, err := c.Get(requestURL, options)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return LikesResponse{}, err
 	}
-	var result LikesResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res LikesResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Queue gets posts that are currently in the blog's queue.
@@ -273,26 +256,26 @@ func (c *Client) BlogLikes(blogname string, options url.Values) (LikesResponse, 
 // offset: post number to start at;
 // filter: specify posts' format(e.g. format="html", format="text", format="raw").
 func (c *Client) Queue(blogname string, options url.Values) (DraftsResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/posts/queue", blogname)
-	data, err := c.Get(requestURL, options)
+	reqURL := fmt.Sprintf("/v2/blog/%s/posts/queue", blogname)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return DraftsResponse{}, err
 	}
-	var result DraftsResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res DraftsResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Drafts gets posts that are currently in the blog's drafts.
 // options can be:
 // filter: specify posts' format(e.g. format="html", format="text", format="raw").
 func (c *Client) Drafts(blogname string, options url.Values) (DraftsResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/posts/draft", blogname)
-	data, err := c.Get(requestURL, options)
+	reqURL := fmt.Sprintf("/v2/blog/%s/posts/draft", blogname)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return DraftsResponse{}, err
 	}
-	var result DraftsResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res DraftsResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Submission retrieve submission posts.
@@ -300,26 +283,26 @@ func (c *Client) Drafts(blogname string, options url.Values) (DraftsResponse, er
 // offset: post number to start at;
 // filter: specify posts' format(e.g. format="html", format="text", format="raw").
 func (c *Client) Submission(blogname string, options url.Values) (DraftsResponse, error) {
-	requestURL := fmt.Sprintf("/v2/blog/%s/posts/submission", blogname)
-	data, err := c.Get(requestURL, options)
+	reqURL := fmt.Sprintf("/v2/blog/%s/posts/submission", blogname)
+	data, err := c.Get(reqURL, options)
 	if err != nil {
 		return DraftsResponse{}, err
 	}
-	var result DraftsResponse
-	return result, json.Unmarshal(data.Response, &result)
+	var res DraftsResponse
+	return res, json.Unmarshal(data.Response, &res)
 }
 
 // Follow the url of a given blog.
 // blogname: the url of the blog to follow.
 func (c *Client) Follow(blogname string) error {
-	requestURL := fmt.Sprintf("/v2/user/follow")
+	reqURL := fmt.Sprintf("/v2/user/follow")
 	params := url.Values{"url": []string{blogname}}
-	data, err := c.Post(requestURL, params)
+	data, err := c.Post(reqURL, params)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 200 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -327,14 +310,14 @@ func (c *Client) Follow(blogname string) error {
 // Unfollow the url of a given blog.
 // blogname: the url of the blog to unfollow.
 func (c *Client) Unfollow(blogname string) error {
-	requestURL := fmt.Sprintf("/v2/user/unfollow")
+	reqURL := fmt.Sprintf("/v2/user/unfollow")
 	params := url.Values{"url": []string{blogname}}
-	data, err := c.Post(requestURL, params)
+	data, err := c.Post(reqURL, params)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 200 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -343,14 +326,14 @@ func (c *Client) Unfollow(blogname string) error {
 // id: the id of the post you want to like.
 // reblog_key: the reblog key for the post id.
 func (c *Client) Like(id, reblogKey string) error {
-	requestURL := fmt.Sprintf("/v2/user/like")
+	reqURL := fmt.Sprintf("/v2/user/like")
 	params := url.Values{"id": []string{id}, "reblog_key": []string{reblogKey}}
-	data, err := c.Post(requestURL, params)
+	data, err := c.Post(reqURL, params)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 200 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -359,14 +342,14 @@ func (c *Client) Like(id, reblogKey string) error {
 // id: the id of the post you want to unlike.
 // reblog_key: the reblog key for the post id.
 func (c *Client) Unlike(id, reblogKey string) error {
-	requestURL := fmt.Sprintf("/v2/user/unlike")
+	reqURL := fmt.Sprintf("/v2/user/unlike")
 	params := url.Values{"id": []string{id}, "reblog_key": []string{reblogKey}}
-	data, err := c.Post(requestURL, params)
+	data, err := c.Post(reqURL, params)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 200 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -386,14 +369,14 @@ func (c *Client) Unlike(id, reblogKey string) error {
 // link: the 'click-through' url for the photo;
 // *source: the photo source url.
 func (c *Client) CreatePhoto(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "photo")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -412,14 +395,14 @@ func (c *Client) CreatePhoto(blogname string, options url.Values) error {
 // title: the optional title of the post;
 // *body: the full text body.
 func (c *Client) CreateText(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "text")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -438,14 +421,14 @@ func (c *Client) CreateText(blogname string, options url.Values) error {
 // *quote: the full text of the quote;
 // source: the cited source of the quote.
 func (c *Client) CreateQuote(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "quote")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -465,14 +448,14 @@ func (c *Client) CreateQuote(blogname string, options url.Values) error {
 // *url: the link you are posting;
 // description: the description of the link you are posting.
 func (c *Client) CreateLink(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "link")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -491,14 +474,14 @@ func (c *Client) CreateLink(blogname string, options url.Values) error {
 // title: the title of the chat;
 // *conversation: the text of the conversation/chat, with dialogue labels.
 func (c *Client) CreateChatPost(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "chat")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -517,14 +500,14 @@ func (c *Client) CreateChatPost(blogname string, options url.Values) error {
 // caption: the caption of the post;
 // *externalURL: the url of the site that hosts the audio file.
 func (c *Client) CreateAudio(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "audio")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -543,14 +526,14 @@ func (c *Client) CreateAudio(blogname string, options url.Values) error {
 // caption: the caption for the post;
 // *embed: the html embed code for the video.
 func (c *Client) CreateVideo(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post", blogname)
 	options.Set("type", "video")
-	data, err := c.Post(requestURL, options)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -562,13 +545,13 @@ func (c *Client) CreateVideo(blogname string, options url.Values) error {
 // *id: the id of the reblogged post;
 // *reblog_key: the reblog key of the rebloged post.
 func (c *Client) Reblog(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post/reblog", blogname)
-	data, err := c.Post(requestURL, options)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post/reblog", blogname)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 201 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -577,14 +560,14 @@ func (c *Client) Reblog(blogname string, options url.Values) error {
 // blogname: the url of the blog you want to delete from.
 // id: the id of the post you want to delete.
 func (c *Client) DeletePost(blogname, id string) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post/delete", blogname)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post/delete", blogname)
 	params := url.Values{"id": []string{id}}
-	data, err := c.Post(requestURL, params)
+	data, err := c.Post(reqURL, params)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 200 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
@@ -602,13 +585,13 @@ func (c *Client) DeletePost(blogname, id string) error {
 // *id: the id of the post.
 // The other options are specific to the type of post you want to edit.
 func (c *Client) EditPost(blogname string, options url.Values) error {
-	requestURL := fmt.Sprintf("/v2/blog/%s/post/edit", blogname)
-	data, err := c.Post(requestURL, options)
+	reqURL := fmt.Sprintf("/v2/blog/%s/post/edit", blogname)
+	data, err := c.Post(reqURL, options)
 	if err != nil {
 		return err
 	}
 	if data.Meta.Status != 200 {
-		return errors.New(data.Meta.Msg)
+		return fmt.Errorf(data.Meta.Msg)
 	}
 	return nil
 }
